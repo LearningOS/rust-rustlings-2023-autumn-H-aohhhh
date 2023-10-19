95a0e78725b00165463f33a68dfd256f229beae0
H-aohhhh <1254861532@qq.com>
2023-10-19 23:15:37 +0800

    update

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -5,14 +5,16 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    let last_char = get_char(data.clone());
 
-    string_uppercase(&data);
+    let uppercased_data = string_uppercase(data);
+
+   
 }
 
 // Should not take ownership
@@ -21,8 +23,11 @@ fn get_char(data: String) -> char {
 }
 
 // Should take ownership
-fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+fn string_uppercase(mut data: String) -> String{
+    data = data.to_uppercase();
 
     println!("{}", data);
+
+    data
+
 }
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (costs:i32) ->i32 {
+    if costs <= 40 && costs>0{
+        costs*2
+    }else if costs>40{
+        costs
+    }else{
+        -1
+    }
+}
 
 // Don't modify this function!
 #[test]
